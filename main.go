3075a020090f8a65f4044d29f48d653d40aedcd8
balamh <chandjobala@gmail.com>
2023-08-31 12:07:39 +0530

    add

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	// pb "github.com/netxd/project1/netxd_customer"
+	"google.golang.org/grpc"
+)
+func main(){
+	connection,err:=grpc.Dial("localhost:5001",grpc.WithInsecure())
+	if err!=nil{
+		log.Fatalf("failed to connect:%v",err)
+	}
+	defer connection.Close()
+	client :=pb.NewCustomerServiceClient(connection)
+	response,err :=client.CreateCustomer(context.Background(),&pb.Customer{
+		CustomerId: 1234,
+		FirstName: "bala",
+		LastName: "murugan",
+		BankId: 123674,
+		Balance: 25000,
+		IsActive: true,
+	})
+	if err!=nil{
+		log.Fatalf("failed to call:%v",err)
+	}
+	fmt.Printf("Response:%s\n",response)
+}
